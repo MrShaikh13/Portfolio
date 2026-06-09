1f4ab14f922e470887f362976be01dc5ae309224
MrShaikh13 <musharrafr878@gmail.com>
2026-06-09 20:18:20 +0530

    changed email

--- backend/services/emailService.js
+++ backend/services/emailService.js
@@ -3,7 +3,8 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
   secure: false,
   auth: {
     user: process.env.EMAIL_USER,
