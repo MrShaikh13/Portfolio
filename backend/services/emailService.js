88f1aef9f67c10851589094c52cd6ad7e0a30d4c
MrShaikh13 <musharrafr878@gmail.com>
2026-06-09 20:08:41 +0530

    changed email

--- backend/services/emailService.js
+++ backend/services/emailService.js
@@ -11,6 +11,14 @@ const transporter = nodemailer.createTransport({
   },
 });
 
+transporter.verify((error, success) => {
+  if (error) {
+    console.log("SMTP ERROR:", error);
+  } else {
+    console.log("SMTP READY");
+  }
+});
+
 export const sendEmail = async (name, email, message) => {
   console.log("EMAIL_USER:", process.env.EMAIL_USER);
   console.log("EMAIL_PASS exists:", !!process.env.EMAIL_PASS);
